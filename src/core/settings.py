04ed25e8308933f7fa48e2f292333d12cf173208
Glenn <glenn@not-tech.support>
2023-10-01 13:43:08 +0000

    fix: enable hash for static files
    Signed-off-by: Glenn <glenn@not-tech.support>

--- src/core/settings.py
+++ src/core/settings.py
@@ -137,6 +137,8 @@ STATICFILES_DIRS = [
   BASE_DIR / "static",
 ]
 
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
